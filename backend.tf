f1ee0d5fcb7c5a5bcd751b6a8fe9cdaa88d675ee
ashishjullia19 <ashishjullia19@gmail.com>
2023-01-08 05:29:10 -0500

    updated backend vars

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket = "terraform-ashishjullia"
-    key    = "test-tf-backend/terraform.tfstate"
-    region = "us-east-1"
+    # bucket = "terraform-ashishjullia"
+    # key    = "test-tf-backend/terraform.tfstate"
+    # region = "us-east-1"
 
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "tfstate-tf-test-s3-ashish"
-    encrypt        = true
+    # dynamodb_table = "tfstate-tf-test-s3-ashish"
+    encrypt = true
   }
 }
